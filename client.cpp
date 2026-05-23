9dba1f98b96f97630507bbb0c1c5071f7fddc47e
Amrit Pant <info.amritpant@gmail.com>
2026-05-23 14:21:41 +0545

    codecrafters submit [skip ci]

---
+++ client.cpp
@@ -0,0 +1,33 @@
+#include <cstring>
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main() {
+  // creating socket
+  int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+  // specifying address
+  sockaddr_in serverAddress;
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_port = htons(6379);
+  serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+  // sending connection request
+  connect(clientSocket, (struct sockaddr *)&serverAddress,
+          sizeof(serverAddress));
+
+  // sending data
+  const char *message = "PING";
+  send(clientSocket, message, strlen(message), 0);
+
+  // Receving the data 
+  char reply[1024] = {0};
+  recv(clientSocket, (char *) &reply, sizeof(reply), 0);
+  std::cout << "Reply: " << reply << "\n";
+
+  // closing socket
+  close(clientSocket);
+  return 0;
+}
