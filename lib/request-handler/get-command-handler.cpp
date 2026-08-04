97c451d75b99a35c67b909318b8e712c0e585aa4
Ayurva Pradhananga <ayurvapradhananga@gmail.com>
2026-08-04 01:18:54 +0000

    fix: Nested Multi Error Fixed

--- lib/request-handler/get-command-handler.cpp
+++ lib/request-handler/get-command-handler.cpp
@@ -5,12 +5,15 @@
 std::string get_command_handler(const std::string &key) {
     auto &store = get_store();
     auto it = store.find(key);
+
     if (it==store.end()) {
         return "$-1\r\n"; // RESP Null Bulk String
     }
+
     if(is_expired(it->second)) {
         store.erase(it);
         return "$-1\r\n"; // RESP Null Bulk String
     }
+    
     return bulk_string_resp(it->second.value); // RESP Bulk String
 }
--- lib/request-handler/multi-command-handler.cpp
+++ lib/request-handler/multi-command-handler.cpp
@@ -1,6 +1,9 @@
 #include "../../include/request-handler/multi-command-handler.h"
 
 std::string multi_command_handler(ClientState &client) {
+    if(client.in_multi) {
+        return "-ERR MULTI calls can not be nested\r\n";
+    }
     client.in_multi = true;
     return "+OK\r\n";
 }
